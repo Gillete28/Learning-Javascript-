ad45cb236d41ae2dda98825048cfdefef27e4477
Gillete Nevado <nevadogillete@gmail.com>
2018-10-08 14:38:12 -0700

    learning JavaScript

---
+++ script.js
@@ -0,0 +1,47 @@
+/** Variable mutation and type correction */
+/*
+var firstName = 'Gillete';
+console.loh(firstName);
+var age = 25;
+
+var lastName = 'Nevado';
+var age = 25;
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var jobs, isMarried;
+job = 'Software Engineer';
+isMarried = false;
+
+
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married?' + isMarried);
+
+// Variable mutation
+age = 'twenty five';
+job = 'Software Engineer';
+lastName = 'Nevado';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his lastName?');
+console.log(LastName + ' ' + lastName);
+
+/****
+ * Basic operations
+ ************/
+ageGillete = 25;
+ageFrank = 27;
+ now = 2018;
+ var yearGillete = now - 25;
+ var yearFrank = now - 27;
+ console.log (yearGillete);
+ console.log (yearFrank);
+
+
+ //* Logic Operators *//
+
+ yearGillete = now - ageGillete;
+
+ var GilleteOlder = ageGillete > ageFrank;
+ console.log(GilleteOlder);
